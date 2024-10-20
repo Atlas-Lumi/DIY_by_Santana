9d50e15bfd3e4d8868d7dcf5a650605044ac1031
Digitalzombie <blackdevil32@gmx.net>
2024-10-20 15:09:00 +0200

    cleanup

--- Code/Properties/AssemblyInfo.cs
+++ Code/Properties/AssemblyInfo.cs
@@ -1,25 +1,10 @@
+using DiyBySantana;
 using MelonLoader;
 using System.Reflection;
 
-//This is a C# comment. Comments have no impact on compilation.
-
-[assembly: AssemblyTitle(BuildInfo.ModName)]
-[assembly: AssemblyCopyright($"Created by ModAuthor")]
-
-[assembly: AssemblyVersion(BuildInfo.ModVersion)]
-[assembly: AssemblyFileVersion(BuildInfo.ModVersion)]
-[assembly: MelonInfo(typeof(DiyBySantana.Implementation), BuildInfo.ModName, BuildInfo.ModVersion, BuildInfo.ModAuthor)]
-
-//This tells MelonLoader that the mod is only for The Long Dark.
+[assembly: AssemblyTitle("DiyBySantana")]
+[assembly: AssemblyCopyright("STMSantana, Atlas Lumi")]
+[assembly: AssemblyVersion("2.4.0")]
+[assembly: AssemblyFileVersion("2.4.0")]
+[assembly: MelonInfo(typeof(Implementation), "DiyBySantana", "2.4.0", "STMSantana, Atlas Lumi", null)]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
-
-internal static class BuildInfo
-{
-	internal const string ModName = "DIY by Santana";
-	internal const string ModAuthor = "STMSantana, Atlas Lumi";
-	/// <summary>
-	/// Version numbers in C# are a set of 1 to 4 positive integers separated by periods.
-	/// Mods typically use 3 numbers. For example: 1.2.1
-	/// </summary>
-	internal const string ModVersion = "2.0.0";
-}
